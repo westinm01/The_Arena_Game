c95d76a131c903031f2700c668ca113fcb997c80
muneebghori <mghor004@ucr.edu>
2021-02-24 10:58:48 -0800

    menu.hpp and mainMenu.cpp progress

--- GameMenu/menu.hpp
+++ GameMenu/menu.hpp
@@ -1,5 +1,5 @@
-#ifndef __MAINMENU_HPP__
-#define __MAINMENU_HPP__
+#ifndef __MENU_HPP__
+#define __MENU_HPP__
 #include <iostream>
 #include <string>
 #include <vector> 
@@ -7,13 +7,20 @@ using namespace std;
 
 class Menu{
     Menu(){};
-    virtual ~Menu(){};
+    //virtual ~Menu(){};
     virtual void printMenu() = 0;
     virtual void retrieveInput() = 0;
 };
 
 class MainMenu : public Menu{
 public:
+    vector <Menu*> menuChoices;
+    MainMenu{
+	menuChoices.push_back(Menu* inventory);
+	menuChoices.push_back(Menu* character);
+	menuChoices.push_back(Menu* shop);
+	menuChoices.push_back(Menu* settings);
+}
     virtual void printMenu(){
 	cout << "-= Main Menu =-" << endl;
 	cout << "1. Inventory" << endl;
@@ -22,44 +29,49 @@ public:
 	cout << "4. Settings" << endl;
 	cout << "5. Quit Game" << endl;	    
     }
-    vector <Menu*> menuChoices;
-
-    virtual void retrieveInput(string choice){
+     
+/*    virtual void retrieveInput(string choice){
 	if (choice == "1"){
-	    inventory();   
+	    //inventory();   
 	}
 	if (choice == "2") {
-	    character();
+	    //character();
 	}
 	if (choice == "3") {
-	    sdhop();
+	    shop();
 	}
 	if (choice == "4") {
 	    settings();
 	}
 	if (choice == "5") {
-	    quitGame();
+	    //quitGame();
 	}
     }	
-
 };
+
 class Settings : public Menu{
-    virtual void printMenu(){
+public:   
+    Settings() {}
+ 
+   virtual void printMenu(){
 	cout << "-= Settings =-" << endl;
-	cout << "1. Harness" << endl;
+	cout << "1. Difficulty" << endl;
 	cout << "2. Back" << endl;
     }   
     virtual void retrieveInput(string choice){
 	if (choice == "1"){
-	    hardness();
+	    //hardness();
 	}
 	if (choice == "2"){
-	    back();
+	    //back();
 	}
     }
 };  
 
 class Shop : public Menu{
+public:    
+    Shop() {}
+
     virtual void printMenu(){
 	cout << "-= Shop =-" << endl;
 	cout << "1. Browse" << endl;
@@ -68,12 +80,12 @@ class Shop : public Menu{
 
     virtual void retrieveInput(string choice){
 	if (choice == "1"){
-            browse();
+     //       browse();
 	}
 	if (choice == "2"){
-	    back();
+//	    back();
 	}
     }
 }; 
-
+*/
 #endif
---
+++ GameMenu/menuMain.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "menu.hpp"
+using namespace std;
+
+int main(){
+    Menu test;
+}
+
