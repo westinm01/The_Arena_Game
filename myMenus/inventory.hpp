66f9f011e5a7833419609a8ab03391f40f32b3be
bmcghee98 <coinnpurse@gmail.com>
2021-03-10 01:31:48 -0800

    Fixed error with choice == 0

--- myMenus/inventory.hpp
+++ myMenus/inventory.hpp
@@ -19,7 +19,7 @@ public:
 		cout << "-= Inventory =-" << endl;
 		c->showItems(); 
 		cout << endl;
-		cout << "[Type 0 to exit]" << endl << endl;
+		cout << "[Type 0 to go back]" << endl << endl;
 	}   
 
 	virtual void menuFunc(Character* c){
@@ -28,36 +28,34 @@ public:
 			printMenu(c);
 			cout << "Select an item or exit inventory: ";
 			cin >> choice;
-
-			if(c->isHolding() == false){
-				std::string choice3;
-				cout << "EQUIP or DELETE?(E/D): " << endl;
-				cin >> choice3;
-				cout << endl;
-				
-				if(choice3 == "E"){
-						c->equipItem(choice);
-					}  else if (choice3 == "D"){
-						c->deleteItem(choice);
-						cout << endl;
-						cout << "Reloading menu..." << endl;
-						cout << endl;
-					}
+		
+			if(choice == 0){
+				break;
 			}
 
-				else if(c->isHolding() == true){
-					if(c->getHand()->getName() == c->getItem(choice)->getName()){
-						std::string choice2;
-						cout << c->getHand()->getName() << " is equipped. Do you want to unequip?(Y/N): ";
-						cin >> choice2;
-						cout << endl;
-						if(choice2 == "Y"){
-							c->unequipItem(choice);
-						} else if (c->getHand()->getName() != c->getItem(choice)->getName()){
-							cout << "You already have an item equipped. You must unequip before you select another." << endl;
-						}
-					}
+			if(c->isHolding() == true && c->getHand()->getName() == c->getItem(choice)->getName()){
+				std::string choice2;
+				cout << c->getHand()->getName() << " is equipped. Do you want to unequip?(Y/N): ";
+				cin >> choice2;
+				cout << endl;
+				if(choice2 == "Y" || choice2 == "y"){
+					c->unequipItem(choice);
+				} 
+			} else if(c->isHolding() == false){
+				std::string choice3;
+				cout << "You have selected " << c->getItem(choice)->getName() << ". EQUIP or DELETE?(E/D): ";
+				cin >> choice3;
+				cout << endl;
+				if(choice3 == "D" || choice3 == "d"){
+					c->deleteItem(choice);
+					cout << "Reloading items..." << endl;
+					cout << endl;
+				} else if (choice3 == "E" || choice3 == "e"){
+					c->equipItem(choice);
 				}
+
+			}
+			
 
 		}while (choice != 0);
 	}
